27a181153b43208dc7d9af20004534e91d46fc78
Neil Hemnani <hemnanineil@gmail.com>
2020-06-13 00:22:47 -0400

    added html/css for form + fixed a few bugs

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,18 @@
 $(document).ready(function() {
     $(".toggle-button").click(function() {
         $(".nav-links").toggle();
-    })
+    });
 
     $(".nav-links").click(function() {
         var w = $(window).width();
         if (w < 768) {
             $(".nav-links").hide();
         }
+    });
+
+    $('a[href*="#"]').on('click', function(e) {
+        e.preventDefault()
+        $('html, body').animate( { scrollTop: $($(this).attr('href')).offset().top - 65 }, 300)
     })
 
     $(window).resize(function(){  
